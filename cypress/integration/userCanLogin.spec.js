47945308609ae18a4827c0f3ee4735efc9dfd02e
miyesier <miyesier@gmail.com>
2019-10-17 13:42:24 +0200

    Creates fixtures-file with json-respone to mock out respone from API.

--- cypress/integration/userCanLogin.spec.js
+++ cypress/integration/userCanLogin.spec.js
@@ -1,6 +1,15 @@
 describe('User can log in', () => {
   it('successfully', () => {
     cy.visit('http://localhost:3001');
+    cy.server();
+    cy.route({
+      method: 'POST',
+      url: 'http://localhost:3000/api/v1/auth/sign_in',
+      response: 'fixture:login.json',
+      headers: {
+        "uid": "user@mail.com"
+      }
+    })
     cy.get('#login').click();
     cy.get('#login-form').within(() => {
       cy.get('#email').type('user@mail.com')
@@ -12,6 +21,18 @@ describe('User can log in', () => {
 
   it('with invalid credentials', () => {
     cy.visit('http://localhost:3001');
+    cy.server();
+    cy.route({
+      method: 'POST',
+      url: 'http://localhost:3000/api/v1/auth/sign_in',
+      status: "401",
+      response: {
+        "errors": [
+          "Invalid login credentials. Please try again."
+        ],
+        "success": false
+      }     
+    })
     cy.get('#login').click();
     cy.get('#login-form').within(() => {
       cy.get('#email').type('user@mail.com')
