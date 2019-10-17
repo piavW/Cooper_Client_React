556e78e43907214eaeef37fa46f920d33531b8a5
miyesier <miyesier@gmail.com>
2019-10-17 16:15:50 +0200

    Creates component for displaying saved data, imports it to app.js.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import DisplayCooperResult from './Components/DisplayCooperResult';
 import InputFields from './Components/InputFields';
 import LogInForm from './Components/LogInForm';
 import { authenticate } from './Modules/Auth';
+import DisplayPerformanceData from './Components/DisplayPerformanceData';
 
 class App extends Component {
   constructor(props) {
@@ -38,18 +39,41 @@ class App extends Component {
   }
 
   entryHandler() {
-    this.setState({ entrySaved: true });
+    this.setState({ entrySaved: true, updateIndex: true });
+  }
+
+  indexUpdated() {
+    this.setState({ updateIndex: false });
   }
 
  render() {
   let renderLogin;
   let user;
+  let performanceDataIndex;
 
   if (this.state.authenticated === true) {
     user = JSON.parse(sessionStorage.getItem('credentials')).uid;
     renderLogin = (
       <p>Hi {user}</p>
     )
+    performanceDataIndex = (
+      <button id="show-index" onClick={() => this.setState({ renderIndex: true })}>Show past entries</button>
+    )
+    if (this.state.renderIndex === true) {
+      performanceDataIndex = (
+        <>
+          <DisplayPerformanceData
+            updateIndex={this.state.updateIndex}
+            indexUpdated={this.indexUpdated.bind(this)}
+            />
+            <button onClick={() => this.setState({ renderIndex: false })}>Hide past entries</button>
+        </>
+      )
+    } else {
+      performanceDataIndex = (
+        <button id="show-index" onClick={() => this.setState({ renderIndex: true })}>Show past entries</button>
+      )
+    }
   } else {
     if (this.state.renderLoginForm === true) {
       renderLogin = (
@@ -84,7 +108,7 @@ class App extends Component {
           entrySaved={this.state.entrySaved}
           entryHandler={this.entryHandler.bind(this)}
         />
-          
+          {performanceDataIndex}
         <div>
           {renderLogin}
         </div>
